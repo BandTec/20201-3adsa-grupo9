0e23ba1e691bf4af1ec43f3fb63418587b827cff
matheuscostabandtec <matheus.costa@bandtec.com.br>
2020-09-07 17:09:09 -0300

    add backend do projeto

---
+++ backEnd/jobby-jobs/jobby-jobs/src/main/java/jobbyjobs/com/jobbyjobs/JobbyJobsApplication.java
@@ -0,0 +1,13 @@
+package jobbyjobs.com.jobbyjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbyJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbyJobsApplication.class, args);
+	}
+
+}
---
+++ backEnd/jobby-jobs/jobby-jobs/src/test/java/jobbyjobs/com/jobbyjobs/JobbyJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package jobbyjobs.com.jobbyjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbyJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
