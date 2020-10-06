88736f3a61d7da89f81c418aef27336be73705c7
Brunagd <buna.gd01@gmail.com>
2020-10-06 17:22:45 -0300

    assistente cadastro - prestador de serviço

---
+++ front-end/src/pages/Cadastro/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './cadastro.css';
+import AssistenteHorizontal from '../../components/Assistente';
+
+function Cadastro(){
+    return(
+        <div>
+            <AssistenteHorizontal>
+
+            </AssistenteHorizontal>
+            
+        </div>
+    )
+}
+
+
+
+export default Cadastro;
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home}/>
+                <Route path="/Cadastro" exact component={Cadastro}/>
             </Switch>
         </BrowserRouter>
     );
