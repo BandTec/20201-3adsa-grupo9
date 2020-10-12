31299d4e91ece8c9b45de69c697322599deda0d4
Brunagd <buna.gd01@gmail.com>
2020-10-12 11:11:12 -0300

    cadastro

--- front-end/src/pages/Cadastro/index.js
+++ front-end/src/pages/Cadastro/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './cadastro.css';
 import AssistenteHorizontal from '../../components/Assistente';
+import Formulario from '../../components/Formulario';
+
+
 
 function Cadastro(){
     return(
         <div>
-            <AssistenteHorizontal>
-
-            </AssistenteHorizontal>
+            <h1>Cadastre-se já e aumente sua clientela</h1>
+            <h2>Este é o passo 1</h2> 
+            
+            <Formulario>
+            </Formulario>
+            
             
         </div>
     )
