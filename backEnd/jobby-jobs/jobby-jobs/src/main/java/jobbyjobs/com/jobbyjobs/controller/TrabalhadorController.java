ea57747b9ae89f1e1266ff195e00a6ba818d668a
matheuscostabandtec <matheus.costa@bandtec.com.br>
2020-11-16 20:14:33 -0300

    add: ajustes em arquivos e testes unitarios

--- backEnd/jobby-jobs/jobby-jobs/src/main/java/jobbyjobs/com/jobbyjobs/controller/TrabalhadorController.java
+++ backEnd/jobby-jobs/jobby-jobs/src/main/java/jobbyjobs/com/jobbyjobs/controller/TrabalhadorController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 
+import static org.springframework.http.ResponseEntity.*;
+
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/trabalhadores")
@@ -40,14 +42,16 @@ public class TrabalhadorController implements calcularSalario {
     private List<Login> logados = new ArrayList<>();
 
     @GetMapping
-    public ResponseEntity getTrabalhadores(@RequestParam(required = false) Integer id){
-        if(profissionalRepository.count() == 0){
-            return ResponseEntity.noContent().build();
-        } else if (id == null){
-            return ResponseEntity.ok(profissionalRepository.findAll());
-        } else {
-            return ResponseEntity.ok(profissionalRepository.findById(id));
-        }
+    public ResponseEntity getTrabalhadores(){
+        List<Profissional> profissionais = profissionalRepository.findAll();
+        return profissionais.isEmpty()
+            ? noContent().build()
+            : ok(profissionais);
+    }
+
+    @GetMapping("{/id}")
+    public ResponseEntity getTrabalhadorById(@PathVariable Integer id){
+        return of(profissionalRepository.findById(id));
     }
 
     @PostMapping
@@ -56,7 +60,7 @@ public class TrabalhadorController implements calcularSalario {
             babaRepository.save(p.getBaba());
         }
         profissionalRepository.save(p);
-        return ResponseEntity.status(201).build();
+        return status(201).build();
     }
 
 
@@ -65,10 +69,10 @@ public class TrabalhadorController implements calcularSalario {
         for (Usuario u: userRepository.findAll()){
             if (l.getEmail().equals(u.getEmail()) && l.getSenha().equals(u.getSenha())){
                 logados.add(l);
-                return ResponseEntity.ok("Login Aceito!");
+                return ok("Login Aceito!");
             }
         }
-        return ResponseEntity.status(404).body("E-mail ou senha incorretos.");
+        return status(404).body("E-mail ou senha incorretos.");
     }
 
     @PostMapping("/logoff")
@@ -77,45 +81,21 @@ public class TrabalhadorController implements calcularSalario {
             Login logado = logados.get(i);
             if(logado.getEmail().equals(email)){
                 logados.remove(i);
-                return ResponseEntity.ok().build();
+                return ok().build();
             }
         }
-        return ResponseEntity.status(404).build();
+        return status(404).build();
     }
 
     @GetMapping("/logados")
     public ResponseEntity getLogados() {
-        return this.logados.isEmpty() ? ResponseEntity.status(204).build() : ResponseEntity.ok(this.logados);
+        return this.logados.isEmpty() ? status(204).build() : ok(this.logados);
     }
 
-    @GetMapping("salario-trabalhador")
-    @Override
-    public Double calcularSalarioTrabalhador(
-            @RequestParam(required = true) int id,
-            @RequestParam(required = true) int qtdHoras,
-            @RequestParam(required = false) Double valorMetro,
-            @RequestParam(required = false) Double qtdMetros) {
-        Optional<Profissional> trabalhador = profissionalRepository.findById(id);
-        double salario;
-        if (valorMetro != null && qtdMetros != null){
-           if(trabalhador.get().getCobrarPorHora()){
-               salario = (trabalhador.get().getValorHora() * qtdHoras) + (valorMetro * qtdMetros);
-           } else {
-               salario = ((trabalhador.get().getValorFixo()) + ((valorMetro * qtdMetros)));
-           }
-        } else {
-            if(trabalhador.get().getCobrarPorHora()){
-                salario = (trabalhador.get().getValorHora() * qtdHoras);
-            } else {
-                salario = (trabalhador.get().getValorFixo());
-            }
-        }
-        return salario;
-    }
 
     @GetMapping("salario-baba")
     @Override
-    public Double calcularSalarioBaba(
+    public ResponseEntity calcularSalarioBaba(
         @RequestParam(required = true) int id,
         @RequestParam(required = true) int idadeMedia,
         @RequestParam(required = false) int qtdCriancas,
@@ -143,7 +123,7 @@ public class TrabalhadorController implements calcularSalario {
             valorCobrado +=  t.get().getBaba().getPrecoLimpeza();
         }
 
-        return valorCobrado;
+        return ok(valorCobrado * qtdCriancas);
 
     }
 
@@ -153,9 +133,9 @@ public class TrabalhadorController implements calcularSalario {
 
         if (babaExistente.isPresent()) {
             List<Notifcacoes> notifcacoes  = notificacaoRepository.findByBabaNotificadaId(id);
-            return ResponseEntity.ok(notifcacoes);
+            return ok(notifcacoes);
         } else {
-            return ResponseEntity.notFound().build();
+            return notFound().build();
         }
     }
 
